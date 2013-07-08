2ee6200b6530d88caa6c4772cfb914b696c103f5
choonq <choonq@gmail.com>
2013-07-08 17:58:29 +0900

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PentaScheduler::Application.config.secret_key_base = '4abcdf5d4a201090cd67170ff2eccc0b05d71a67ae1af8b457f9f814f8fa79a1ace219e6ab73d91034fb4bffce2c7cdc1db2b36aa6caac8eff4170e79b76713f'
